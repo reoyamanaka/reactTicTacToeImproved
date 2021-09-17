983581589b628a6d21954ec90548c520e3947211
Reo Yamanaka <ryamanaka807@gmail.com>
2021-09-16 21:14:09 -0400

    separated each component into its own js file

---
+++ src/Square.js
@@ -0,0 +1,10 @@
+// if a component has a "render" method, just write it as a function component instead of as a class component
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+export default Square;
